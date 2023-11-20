bcf5fd67d2439cb305150ae12c5a6b799f606927
Ubuntu <azureuser@devVM.ir004aqj314uzfnxh1yfpc4efb.lx.internal.cloudapp.net>
2023-11-20 21:36:33 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -15,8 +15,11 @@ function App() {
     xhr.open("GET", "https://webchat.botframework.com/api/tokens", true);
     xhr.setRequestHeader("Authorization", "BotConnector " + '9btLS8G3L9w.poDHGSZtpzG9d1aYEtc5wZvXXAGnLhjRqB-L4sYhcgc');
     xhr.send();
+    xhr.onreadystatechange = processRequest;
+    function processRequest(e) {
         var response = JSON.parse(xhr.responseText);
         document.getElementById("chat").src="https://webchat.botframework.com/embed/gr5-Inquiry-ls-bot?t="+response
+    }
 
 </script>
 </html>
