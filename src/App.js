9a89a8098f9fb1cbc238b84f15f372efc8d2ca6c
Ubuntu <azureuser@devVM.ir004aqj314uzfnxh1yfpc4efb.lx.internal.cloudapp.net>
2023-11-20 21:39:25 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -16,8 +16,10 @@ function App() {
     xhr.setRequestHeader("Authorization", "BotConnector " + '9btLS8G3L9w.poDHGSZtpzG9d1aYEtc5wZvXXAGnLhjRqB-L4sYhcgc');
     xhr.send();
     xhr.onreadystatechange = processRequest;
+
+    var response;
     function processRequest(e) {
-        var response = JSON.parse(xhr.responseText);
+        response = JSON.parse(xhr.responseText);
         document.getElementById("chat").src="https://webchat.botframework.com/embed/gr5-Inquiry-ls-bot?t="+response
     }
 
