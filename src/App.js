56cab097f43840de398ffd3347d64b36e3d8a4b2
Ubuntu <azureuser@devVM.ir004aqj314uzfnxh1yfpc4efb.lx.internal.cloudapp.net>
2023-11-20 19:18:38 +0000

    Fix

--- src/App.js
+++ src/App.js
@@ -2,31 +2,6 @@ import React from 'react';
 
 function App() {
 
-return (
-
-<html>
-<body>
-  <iframe id="chat" style="width: 400px; height: 400px;" src=''></iframe>
-</body>
-<script>
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "https://webchat.botframework.com/api/tokens", true);
-    xhr.setRequestHeader('Authorization', 'BotConnector ' + '9btLS8G3L9w.poDHGSZtpzG9d1aYEtc5wZvXXAGnLhjRqB-L4sYhcgc');
-    xhr.send();
-    xhr.onreadystatechange = processRequest;
-
-    function processRequest(e) {
-      if (xhr.readyState == 4  && xhr.status == 200) {
-        var response = JSON.parse(xhr.responseText);
-        document.getElementById("chat").src="https://webchat.botframework.com/embed/gr5-Inquiry-ls-bot?t="+response
-      }
-    }
-
-  </script>
-</html>
-
-);
-}
+return ()
 
 export default App;
